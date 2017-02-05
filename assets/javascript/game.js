437ebebf31339b2e4598c5d16ecf02a73cb40eff
atlhaze <mskopell@gmail.com>
2017-02-04 22:13:26 -0500

    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,75 @@
+var wins = 0;
+var losses = 0;
+var lives = 9;
+var picks = [];
+// var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var secret;
+var pick;
 
-var wins;
-var losses;
-var guesses;
-var alphabet [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
-var picks [];
+function compChoice (){
 
+	return alphabet[Math.round(Math.random()*23)];
 
-function compChoice (alphabet){
-	var returnValue = Math.random()*23;
-	console.log(returnValue);
-	return returnValue;
 };
 
-function userChoice (){
+function keyPress (event){
 	
+	displayError("");
+	pick = String.fromCharCode(event.keyCode).toUpperCase();
+
+	if (picks.indexOf(pick) > -1){
+		displayError("You\'ve already tried " + "'" + pick + "'" + "! Try a different letter!");
+		return;
+	};
+
+	picks.push(pick);
+	document.getElementById("guesses").innerHTML = picks;
+	
+	if (pick == secret) {
+		wins++;
+		reset();
+	}else{
+		lives--;
+		if (lives === 0){
+			losses++;
+			reset();
+		};
+
+	}
+	updateScreen();
 };
-// var index = compChoice (alphabet);
-// var secretLetter = alphabet[index];
-var secretLetter = alphabet[compChoice (alphabet)];
 
 
+function reset(){
 
-window.onload=compChoice;
+	picks = [];
+	lives = 9;
+	secret = compChoice();
+	// console.log(secret);
+};
+
+
+function updateScreen(){
+	
+	document.getElementById("losses").innerHTML = losses;
+	document.getElementById("guesses").innerHTML = picks;
+	document.getElementById("lives").innerHTML = lives;
+	document.getElementById("wins").innerHTML = wins;
+};
+
+
+function displayError (error){
+	document.getElementById("errors").innerHTML = error;
+};
+
+
+function execute (){
+
+	secret = compChoice();
+	document.onkeypress = keyPress;
+	// For Cheaters & Testers
+	// console.log(secret);
+};
+
+window.onload = execute;
